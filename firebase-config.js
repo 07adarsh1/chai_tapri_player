71eb6d1a3ddc8bde746661528e9f08be215fddef
Adarsh Singh <142831798+07adarsh1@users.noreply.github.com>
2026-08-15 21:21:12 +0530

    error fix

---
+++ firebase-config.js
@@ -0,0 +1,6 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBiRBuZa7pJRDMh2itgX_OM0RsQMsHlecE",
+  authDomain: "chai-tapri-86c4d.firebaseapp.com",
+  databaseURL: "https://chai-tapri-86c4d-default-rtdb.firebaseio.com",
+  projectId: "chai-tapri-86c4d"
+};
